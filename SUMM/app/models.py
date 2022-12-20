9118309c2123bd1bd5bcca4d6d05a316e9c22533
Benedikt Schwankner <Benedikt_1212@outlook.de>
2022-12-20 22:06:07 +0100

    setup sqllite and created models, Started working on CRUD

--- SUMM/app/models.py
+++ SUMM/app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class user(models.Model):
+    Id = models.IntegerField(primary_key=True)
+    Name = models.CharField(max_length=200)
+    QuotaLimit = models.IntegerField()
+    QuotaSpent = models.IntegerField()
+
+    def __str__(self):
+        return self.Name
+    
+    def get_absolute_url(self):
+        return reverse('user_edit', kwargs={'pk': self.Id})
+
+class translation(models.Model):
+    Id = models.IntegerField(primary_key=True)
+    InputText = models.CharField(max_length=200)
+    OutputText = models.CharField(max_length=200)
+    FromUserId = models.ForeignKey(user, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.Name
+    
+    def get_absolute_url(self):
+        return reverse('translation_edit', kwargs={'pk': self.Id})
