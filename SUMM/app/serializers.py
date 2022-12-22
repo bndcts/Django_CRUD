81b9a35ccf97252f8109a96be55bb59efd019d98
Benedikt Schwankner <Benedikt_1212@outlook.de>
2022-12-22 08:54:49 +0100

    CRUD working(not used generic views)

--- SUMM/app/serializers.py
+++ SUMM/app/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from django.db.models import fields
-from .models import User
+from .models import User, Translation
   
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['Id', 'Name', 'QuotaLimit', 'QuotaSpent']
+
+class TranslationSerializer(serializers.ModelSerializer):
+    #FromUserId = serializers.PrimaryKeyRelatedField(read_only=True)
+    class Meta:
+        model = Translation
+        fields = ('Id', 'InputText', 'OutputText', 'FromUserId')
--- SUMM/app/views.py
+++ SUMM/app/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponse
-from .models import User
+from .models import Translation, User
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
-from .serializers import UserSerializer
+from .serializers import UserSerializer, TranslationSerializer
 from rest_framework import serializers
 from rest_framework import status
 
@@ -14,18 +14,20 @@ def home(request):
 @api_view(['GET'])
 def ApiOverview(request):
     api_urls = {
-        'all_Users': '/',
-        'Add': '/add',
-        'Update': '/update/pk',
-        'Delete': '/user/pk/delete'
+        'all_Users': '/users/all',
+        'Add_user': '/users/add',
+        'Update_user': '/update/pk',
+        'Delete_user': '/users/pk/delete',
+        'all_Translations': '/translations',
+        'Add_translation': '/translations/add',
+        'Update_translation': '/translations/update/pk',
+        'Delete_translation': '/translations/pk/delete'
     }
     return Response(api_urls)
 
 @api_view(['POST'])
 def add_user(request):
-    print(request)
     user = UserSerializer(data=request.data)
-    print(user)
     if User.objects.filter(**request.data).exists():
         raise serializers.ValidationError('This data does already exist.')
     
@@ -69,4 +71,52 @@ def update_users(request, pk):
 def delete_users(request, pk):
     user = get_object_or_404(User, pk=pk)
     user.delete()
+    return Response(status=status.HTTP_202_ACCEPTED)
+
+@api_view(['POST'])
+def add_translation(request):
+    translation = TranslationSerializer(data=request.data)
+    if Translation.objects.filter(**request.data).exists():
+        raise serializers.ValidationError('This data does already exist.')
+    
+    if translation.is_valid():
+        translation.save()
+        return Response(translation.data)
+    else:
+        return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
+
+
+@api_view(['GET'])
+def view_translations(request):
+    
+    # checking for the parameters from the URL
+    if request.query_params:
+        Translations = Translation.objects.filter(**request.query_param.dict())
+    else:
+        Translations = Translation.objects.all()
+  
+    # if there is something in Translations else raise error
+    if Translations:
+        data = TranslationSerializer(Translations, many=True)
+        return Response(data.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['POST'])
+def update_translations(request, pk):
+    translation = Translation.objects.get(pk=pk)
+    data = TranslationSerializer(instance=translation, data=request.data)
+  
+    if data.is_valid():
+        data.save()
+        return Response(data.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['DELETE'])
+def delete_translations(request, pk):
+    translation = get_object_or_404(Translation, pk=pk)
+    translation.delete()
     return Response(status=status.HTTP_202_ACCEPTED)
